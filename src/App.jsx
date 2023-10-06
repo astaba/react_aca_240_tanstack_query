dceaa0b1f999b8af4b5d1a0c858dd6c5762bec54
Astar <bahouidi99@gmail.com>
2023-10-06 12:12:58 +0100

    Install Devtolls

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import Events from "./components/Events/Events.jsx";
 import EventDetails from "./components/Events/EventDetails.jsx";
@@ -44,6 +45,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
